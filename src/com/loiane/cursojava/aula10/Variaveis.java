8f0d283c916c05ce6695a5ee837dfd1ed6e843e7
nova2DOT <86917818+nova2DOT@users.noreply.github.com>
2021-07-06 20:35:32 -0300

    Add files via upload

---
+++ src/com/loiane/cursojava/aula10/Variaveis.java
@@ -0,0 +1,25 @@
+	package com.loiane.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade = 21;
+		String nome = "João";
+		String nomeDoMeuCachorro = "Sol";
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("NomeDoCachorro: " + nomeDoMeuCachorro);
+		
+		// não ajuda. Sempre utilizar algo que significa alguma coisa.
+		int a = 21;
+		String b = "João";
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/com/loiane/cursojava/aula11/Curiosidadeint.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula11;
+
+public class Curiosidadeint {
+
+	public static void main(String[] args) {
+		 	
+		int var1 = 2147483647;
+		
+		int var2 = 100;
+		
+		System.out.println(var1 + var2);
+		
+		
+		//-2147483549
+
+	}
+
+}
